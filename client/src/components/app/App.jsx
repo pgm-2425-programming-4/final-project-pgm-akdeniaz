8a2f21ae17cc3a01d283820078cf93341c0bf46c
pgm-akdeniaz <akdenis.niaziev@student.arteveldehs.be>
2025-05-18 22:26:34 +0200

    Add pagination

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -1,26 +1,17 @@
-import { useEffect, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
+import { PaginatedTaskList } from "./paginated-task-list/paginated-task-list";
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:1337/api/tasks", {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Bearer a78b19ea610f99913032e6eeb0108b63cff073b48984a3eed283d670339c3adbcaac1edf487b688f89de02b8b51bfacdf74a27b68be3e1f99f6d9fc8fa104caaedf97ede5a651d5ed6e0eebe2c73da1c58a80b7b7ebcdad4a6ac85329ba87116e695cb5e91734db3d85514dd72be1894393ad361ab9f8b504a4023ee500a6902",
-      },
-    })
-      .then((data) => {
-        return data.json();
-      })
-      .then((jsonData) => {
-        setTasks(jsonData.data);
-      });
-  }, []);
-
-  return <Tasks tasks={tasks} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div style={{ margin: "2rem" }}>
+        <PaginatedTaskList />
+      </div>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ client/src/components/app/paginated-task-list/task-list/task-list.jsx
@@ -0,0 +1,13 @@
+export function TaskList({ tasks }) {
+  return (
+    <ul>
+      {tasks.map((task) => {
+        return (
+          <li key={task.id}
+          >{`${task.title}`}
+            </li>
+        );
+      })}
+    </ul>
+  );
+}
