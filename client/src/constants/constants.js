7a01095e69ca4c4214c61ca698d37b7f5ebf2118
pgm-akdeniaz <akdenis.niaziev@student.arteveldehs.be>
2025-05-27 09:47:01 +0200

    API url updated

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,5 +1,5 @@
 export const API_URL = import.meta.env.PROD
-  ? "https://jammin-playground.onrender.com/api"
+  ? "https://final-project-pgm-akdeniaz.onrender.com/api"
   : "http://localhost:1337/api";
 export const API_TOKEN = import.meta.env.PROD
   ? "719d1074cd669b1c2f2e1b901f8505d6420bd47cd9c81beef19ffd2a2ac2b1ea7cc2e5369a49404ca3328a8851905c2adacbeaf60857cae39e8801251ba4e8dc5faca77b1f5ffca2e24b50fbf522e589b7e2023fd6fdd6cb4ca67acd0bf51e1e7f12ca985182981862f7af6a2f4d9143280b7dca6a4d27bb42084e3bee391088"
